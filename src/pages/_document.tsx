0433fb5eaa2b51b3a651ada934d1d434631d7fcf
Silésio Leudério Cipriano <silesioleuderiocipriano@hotmail.com>
2023-01-14 10:32:48 +0200

    feat favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,42 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import Document, { Html, Main, NextScript } from 'next/document';
+import Head from 'next/head';
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/images/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/images/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Nunito:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <title>Day one - Memento</title>
         </Head>
         <body>
           <Main />
